159953cc1149516a6d2a453f6cd2e8f605d7290b
ansl-lab <ymah89+ansl@gmail.com>
2026-03-04 05:43:59 +0000

    Deploying to gh-pages from @ ansl-lab/ansl-lab.github.io@2301394eaa9d40bd30458c7a1953570e5043ae5d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-professor",
-          title: "Professor",
+          title: "professor",
           description: "",
           section: "Navigation",
           handler: () => {
